e7b08b091cf29c67df604befaf45d17c9ca24b30
Agustin Sanchez <nitsuga1810@hotmail.com>
2018-10-24 15:38:22 -0300

    punto 2

--- Practica 4/Arduino/src/main.cpp
+++ Practica 4/Arduino/src/main.cpp
@@ -1,9 +1,42 @@
 #include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
 
-void setup() {
-    // put your setup code here, to run once:
+void task(void* pvParameters){
+	while(1){
+		Serial.println((char*)pvParameters);
+		/*
+		Pasa a estado "suspendido" para dejarle tiempo al resto de las tareas,
+		hasta que se cumple ese delay.
+		Esta especificado en ticks, por eso se usa esa constante para hacer
+		la conversion necesaria de ms a ticks.
+		*/
+		vTaskDelay(100/portTICK_PERIOD_MS);
+	}
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
+static void task_idle(void* pvParameters){
+	while(1){
+		Serial.println((char*)pvParameters);
+		vTaskDelay(50/portTICK_PERIOD_MS);
+	}
 }
+
+void setup(){	
+	Serial.begin(9600);
+	/*
+	xTaskCreate(
+		puntero a funcion con codigo de la tarea,
+		nombre de la tarea, 
+		profundidad del stack,
+		argumentos de la funcion de la tarea,
+		prioridad,
+		no se);
+	*/
+	Serial.println("\nStart");		
+	xTaskCreate(task, "task_name_1", 100, (void*)("Task 1"), 3, NULL);
+	xTaskCreate(task, "task_name_2", 100, (void*)("Task 2"), 1, NULL);
+	xTaskCreate(task, "task_name_3", 100, (void*)("Task 3"), 1, NULL);
+	//xTaskCreate(task_idle, "task_name_idle", 100, (void*)("Task IDLE"), 0, NULL);
+}
+
+void loop(){}
